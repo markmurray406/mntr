5b80961afdd90b885474f20e27de82525c92a018
Mark Murray <markmurray406@gmail.com>
2013-05-15 19:44:06 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mntr::Application.config.secret_token = 'b03f0d1d4ed2875793ac4f7dc63b04a5a3ef600c499b2dfb250746e3b2c2ef1477f80da9b12d8a3c6d8c59e31efc5c675dd03ded16927d8f5fa627d57629fa64'
